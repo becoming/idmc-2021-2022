534021745e72f43f8e80b81ebb9f252f857fa5f7
Elouan <elouanbrst@gmail.com>
2021-11-17 15:51:54 +0100

    bean dans le main (en commentaires)

--- src/main/java/fr/unilorraine/idmc/gamescatalog/GamesCatalogApplication.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/GamesCatalogApplication.java
@@ -1,7 +1,10 @@
 package fr.unilorraine.idmc.gamescatalog;
 
+import fr.unilorraine.idmc.gamescatalog.repositories.GamesRepository;
+import fr.unilorraine.idmc.gamescatalog.services.GamesService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GamesCatalogApplication {
@@ -9,5 +12,10 @@ public class GamesCatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GamesCatalogApplication.class, args);
 	}
+
+//	@Bean
+//	public GamesService gameService(GamesRepository repo){
+//		return new GamesService(repo);
+//	}
 
 }
