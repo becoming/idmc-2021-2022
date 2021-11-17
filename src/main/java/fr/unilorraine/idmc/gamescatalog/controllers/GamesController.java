c10a9e871cc94d9595a8147408f5cb9463e3a6df
Stephane <stephane.mairesse54@gmail.com>
2021-11-17 15:16:52 +0100

    Add post games and requests file

--- src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/GamesController.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/GamesController.java
@@ -3,9 +3,7 @@ package fr.unilorraine.idmc.gamescatalog.controllers;
 import fr.unilorraine.idmc.gamescatalog.entities.Game;
 import fr.unilorraine.idmc.gamescatalog.services.GamesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("games")
@@ -17,5 +15,10 @@ public class GamesController {
     @GetMapping
     public Iterable<Game> findAll() {
         return gameService.findAll();
+    }
+
+    @PostMapping
+    public Game create(@RequestBody Game gg) {
+        return gameService.create(gg);
     }
 }
