0180fa9eeec1328311aee5c5f693cf98d8b186c1
Gael Jaman <gaeljaman@MacBook-Pro-de-Gael.local>
2021-11-17 14:45:05 +0100

    ajout réalisations cour1 matin

---
+++ Cour 1/laverDents.java
@@ -0,0 +1,12 @@
+
+// Saisir brosse à dents
+// Laver brosse à dents
+// Saisir dentifrice
+// ouvrir dentifrice
+// appliquer dentifrice sur brosse à dents
+// refermer dentifrice
+// mettre de l'eau sur brosse à dents
+// brosser dents
+// rincer bouche
+// rincer brosse à dents
+// ranger brosse à dents
