9bccc7c046b673931ed64221ff6500ba70a08c8c
Tristan Luc <tristan.luc1@etu.univ-lorraine.fr>
2021-11-24 11:58:51 +0100

    added feign

--- src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/PostmanEchoController.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/PostmanEchoController.java
@@ -1,6 +1,7 @@
 package fr.unilorraine.idmc.gamescatalog.controllers;
 
 import fr.unilorraine.idmc.gamescatalog.client.PostmanEchoClient;
+import fr.unilorraine.idmc.gamescatalog.client.PostmanEchoFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostmanEchoController {
 
     private final PostmanEchoClient client;
+    private final PostmanEchoFeignClient feignClient;
 
     @GetMapping("get")
     public String testGet() {
         return client.testGet();
+    }
+
+    @GetMapping("get/feign")
+    public String testGetFeign() {
+        return feignClient.testGet();
     }
 }
