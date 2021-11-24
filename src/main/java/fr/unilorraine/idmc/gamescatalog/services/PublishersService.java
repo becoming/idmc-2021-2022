cd454b6b8b41c469e2c4689e95472e84178f03b7
Vincent <vincent.choquert0@gmail.com>
2021-11-24 11:25:27 +0100

    publisher dto

--- src/main/java/fr/unilorraine/idmc/gamescatalog/services/PublishersService.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/services/PublishersService.java
@@ -1,21 +1,32 @@
 package fr.unilorraine.idmc.gamescatalog.services;
 
+import fr.unilorraine.idmc.gamescatalog.dto.NewPublisher;
+import fr.unilorraine.idmc.gamescatalog.dto.PublisherView;
 import fr.unilorraine.idmc.gamescatalog.entities.Publisher;
+import fr.unilorraine.idmc.gamescatalog.mappers.GamesMapper;
+import fr.unilorraine.idmc.gamescatalog.mappers.PublishersMapper;
+import fr.unilorraine.idmc.gamescatalog.repositories.GamesRepository;
 import fr.unilorraine.idmc.gamescatalog.repositories.PublisherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PublishersService {
 
     private final PublisherRepository repo;
+    private final PublishersMapper mapper;
 
-    public Publisher create(Publisher p) {
-        return repo.save(p);
+    public List<PublisherView> findAll() {
+        return mapper.toDto(repo.findAll());
     }
 
-    public Iterable<Publisher> findAll() {
-        return repo.findAll();
+    public PublisherView create(NewPublisher newpublisher){
+        var pub = mapper.toEntity(newpublisher);
+        pub = repo.save(pub);
+
+        return mapper.toDto(pub);
     }
 }
