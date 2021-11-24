9f037adf665bf35fe2a1c6e8722e31467601d968
Vellucen <gabriel.couroux33@orange.fr>
2021-11-24 16:00:05 +0100

    FIN CM2 (javascript + FeatureBasedArchitecture)

--- feature-based-architecture/src/main/java/gamescatalog/game/GamesRepository.java
+++ feature-based-architecture/src/main/java/gamescatalog/game/GamesRepository.java
@@ -1,4 +1,4 @@
-package fr.unilorraine.idmc.gamescatalog.repository;
+package fr.unilorraine.idmc.gamescatalog.repositories;
 
 import fr.unilorraine.idmc.gamescatalog.entities.Game;
 import org.springframework.data.repository.CrudRepository;
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GamesRepository extends CrudRepository<Game, Long> {
-
-
 }
--- feature-based-architecture/src/main/java/gamescatalog/postman/PostmanEchoJDK11Client.java
+++ feature-based-architecture/src/main/java/gamescatalog/postman/PostmanEchoJDK11Client.java
@@ -1,6 +1,9 @@
 package fr.unilorraine.idmc.gamescatalog.client;
 
+import fr.unilorraine.idmc.gamescatalog.config.PostmanProperties;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -9,19 +12,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 @Component
-public class PostmanEchoClient {
+@RequiredArgsConstructor
+public class PostmanEchoJDK11Client {
+
+    private final PostmanProperties properties;
 
     @SneakyThrows
-    public String testGet(){
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://postman-echo.com/get"))
+    public String testGet() {
+        var request = HttpRequest.newBuilder()
+                .uri(new URI(properties.getEchoUrl()))
                 .GET()
                 .build();
 
-        HttpResponse<String> response = HttpClient
+        var response = HttpClient
                 .newHttpClient()
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
         return response.body();
     }
+
 }
