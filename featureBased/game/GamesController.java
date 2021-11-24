bba14bcdd9d042c1fc4a7121967839577a7fd340
ColleenCallonego <colleencallonego@gmail.com>
2021-11-24 15:54:23 +0100

    Javascript and featureBased

---
+++ featureBased/game/GamesController.java
@@ -0,0 +1,33 @@
+package fr.unilorraine.idmc.gamescatalog.controllers;
+
+
+import fr.unilorraine.idmc.gamescatalog.dto.GameView;
+import fr.unilorraine.idmc.gamescatalog.dto.NewGame;
+import fr.unilorraine.idmc.gamescatalog.services.GamesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+// https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/
+@RequestMapping("games")
+@RequiredArgsConstructor
+public class GamesController {
+
+    private final GamesService gamesService;
+
+    @GetMapping
+    public List<GameView> findAll() {
+        return gamesService.findAll();
+    }
+
+    @PostMapping
+
+    @ResponseStatus(HttpStatus.CREATED)
+    public GameView create(@RequestBody NewGame gg) {
+        return gamesService.create(gg);
+    }
+}
