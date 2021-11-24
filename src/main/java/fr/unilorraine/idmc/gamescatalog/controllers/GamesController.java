054305068087fbb7024e92917961c45929be51ef
Vincent <vincent.choquert0@gmail.com>
2021-11-24 09:32:57 +0100

    merge jpa relationships

--- src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/GamesController.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/GamesController.java
@@ -1,25 +1,27 @@
 package fr.unilorraine.idmc.gamescatalog.controllers;
 
+import fr.unilorraine.idmc.gamescatalog.dto.GameView;
+import fr.unilorraine.idmc.gamescatalog.dto.NewGame;
 import fr.unilorraine.idmc.gamescatalog.entities.Game;
 import fr.unilorraine.idmc.gamescatalog.services.GamesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+// https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/
 @RequestMapping("games")
 @RequiredArgsConstructor
 public class GamesController {
 
-    private final GamesService gameService;
+    private final GamesService gamesService;
 
     @GetMapping
-    public Iterable<Game> findAll(){
-        return gameService.findAll();
+    public Iterable<Game> findAll() {
+        return gamesService.findAll();
     }
 
     @PostMapping
-    public Game create(@RequestBody Game g){
-        return gameService.create(g);
+    public GameView create(@RequestBody NewGame gg) {
+        return gamesService.create(gg);
     }
-
 }
