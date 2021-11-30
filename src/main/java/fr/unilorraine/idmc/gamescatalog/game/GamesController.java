90181c4f6a06f8eb764fe10e10180c2ffb21ddc3
Clément <clement.mercier@hotmail.com>
2021-11-30 17:52:24 +0100

    ✨ Exercice ajout ParnersControllers

--- src/main/java/fr/unilorraine/idmc/gamescatalog/game/GamesController.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/game/GamesController.java
@@ -1,4 +1,4 @@
-package fr.unilorraine.idmc.gamescatalog.game;
+    package fr.unilorraine.idmc.gamescatalog.game;
 
 import fr.unilorraine.idmc.gamescatalog.game.data.GameView;
 import fr.unilorraine.idmc.gamescatalog.game.data.NewGame;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-// https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/
 @RequestMapping("games")
 @RequiredArgsConstructor
 public class GamesController {
---
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/partners/PartnersController.java
@@ -0,0 +1,56 @@
+package fr.unilorraine.idmc.gamescatalog.partners;
+
+import fr.unilorraine.idmc.gamescatalog.core.config.PartnersProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("partners")
+@RequiredArgsConstructor
+public class PartnersController {
+    private String jwt;
+    private final PartnersProperties properties;
+    private final PartnersFeignClient gamesClient;
+
+    @GetMapping
+    public String getTest() {
+        Map<String, String> headers = new HashMap<>() {{
+            String auth = getAuth(properties.getToken());
+            put("Authorization", "Bearer " + auth);
+            put("Client-ID", properties.getClientid());
+            put("Content-Type", "application/json");
+        }};
+
+        return gamesClient.getTest(headers, "");
+    }
+
+    @SneakyThrows
+    public String getAuth(String token) {
+        if (jwt == null) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(token))
+                    .POST(HttpRequest.BodyPublishers.ofString(""))
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String res = response.body().split(",")[0].split(":")[1];
+            String substring = res.substring(1, res.length() - 1);
+            jwt = substring;
+            return substring;
+        } else {
+            return jwt;
+        }
+    }
+}
