06070c7235936d51a0f57f30e45a274c684cddac
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-12-01 14:17:01 +0100

    added a bit of liquibase

---
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/entities/User.java
@@ -0,0 +1,25 @@
+package fr.unilorraine.idmc.gamescatalog.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+}
