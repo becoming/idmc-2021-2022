37e18bbb18d5bbe430867178250fa03de1610f88
Clément <clement.mercier@hotmail.com>
2021-12-08 10:18:27 +0100

    ✨ Cours du 1/12

--- src/main/java/fr/unilorraine/idmc/gamescatalog/GamesCatalogApplication.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/GamesCatalogApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class GamesCatalogApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GamesCatalogApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GamesCatalogApplication.class, args);
+	}
 
 }
